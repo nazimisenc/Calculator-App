cd0d1863379fe10ed726d1c0cca7f18daa9ac792
Nazım <116541413+nazimisenc@users.noreply.github.com>
2022-12-19 14:01:13 +0300

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,124 @@
+const display = document.querySelector(".calculator-input");
+const keys = document.querySelector(".calculator-keys");
+
+let displayValue ="0";
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
+
+updateDisplay();
+
+function updateDisplay()
+{
+    display.value = displayValue;
+}
+
+keys.addEventListener("click",function(e)
+{
+    const element = e.target;
+
+    if (element.className === "calculator-keys")
+    {
+        return;
+    }
+
+    if (element.classList.contains("operator"))
+    {
+        handleOperator(element.value);
+        updateDisplay();
+        return;
+    }
+
+    if (element.classList.contains("decimal"))
+    {
+        inputDecimal(element.value);
+        updateDisplay();
+        return;
+    }
+
+    if (element.classList.contains("clear"))
+    {
+        clear();
+        updateDisplay();
+        return;
+    }
+
+    inputNumber(element.value);
+    updateDisplay();
+    
+}
+);
+
+function handleOperator(nextOperator)
+{
+    const value = parseFloat(displayValue);
+
+    if (operator && waitingForSecondValue)
+    {
+        operator = nextOperator;
+        return;
+    }
+
+    if (firstValue === null)
+    {
+        firstValue = value;
+    }
+    else if(operator)
+    {
+        const result = calculate(firstValue,value,operator);
+        
+        displayValue = String(result);
+        firstValue = result;
+    }
+
+    waitingForSecondValue = true;
+    operator = nextOperator;
+}
+
+function calculate(first,second,operator)
+{
+    if (operator ==="+")
+    {
+        return first+second;
+    }
+    else if(operator ==="-")
+    {
+        return first-second;
+    }
+    else if(operator ==="*")
+    {
+        return first*second;
+    }
+    else if (operator ==="/")
+    {
+        return first/second;
+    }
+    return second;
+}
+
+function inputNumber(num)
+{
+    if (waitingForSecondValue)
+    {
+        displayValue = num;
+        waitingForSecondValue = false;
+    }
+    else
+    {
+        displayValue = displayValue === "0"? num: displayValue + num;
+    }
+
+}
+
+function inputDecimal()
+{
+    if (!displayValue.includes("."))
+    {
+        displayValue += ".";
+    }
+}
+
+function clear() 
+{
+    displayValue ="0";
+}
